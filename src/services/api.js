d692e087ace0aad1702f74572aededc25de0a444
zikrina <rina1132003@gmail.com>
2025-11-25 20:37:26 +0700

    membenarkan api base

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://backend-bank-belimbing.vercel.app/api";
 
 const apiService = {
-  // Dashboard / General
+  // Dashboard
   getDashboardData: () => axios.get(`${API_URL}/dashboard-data`),
 
   // Customers
-  getCustomers: () => axios.get(`${API_URL}/customers`), // Asumsi ada endpoint get all customers
+  getCustomers: () => axios.get(`${API_URL}/customers`),
   createCustomer: (name) => axios.post(`${API_URL}/customers`, { name }),
   updateCustomer: (id, name) =>
     axios.put(`${API_URL}/customers/${id}`, { name }),
   deleteCustomer: (id) => axios.delete(`${API_URL}/customers/${id}`),
 
   // Deposito Types
-  getTypes: () => axios.get(`${API_URL}/deposito-types`), // Asumsi ada endpoint get all types
+  getTypes: () => axios.get(`${API_URL}/deposito-types`),
   createType: (data) => axios.post(`${API_URL}/deposito-types`, data),
   updateType: (id, data) => axios.put(`${API_URL}/deposito-types/${id}`, data),
   deleteType: (id) => axios.delete(`${API_URL}/deposito-types/${id}`),
 
   // Accounts
-  getAccounts: () => axios.get(`${API_URL}/accounts`), // Asumsi ada endpoint get all accounts
+  getAccounts: () => axios.get(`${API_URL}/accounts`),
   createAccount: (data) => axios.post(`${API_URL}/accounts`, data),
   updateAccount: (id, typeId) =>
     axios.put(`${API_URL}/accounts/${id}`, { deposito_type_id: typeId }),
